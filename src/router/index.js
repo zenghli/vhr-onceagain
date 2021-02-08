9c741382c654cceadff231393e979a7d4a0113ea
zenghl <986850700@qq.com>
2021-02-08 10:55:52 +0800

    项目初始化完成--增加了element-ui的依赖

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Login from '../views/Login';
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login,
+    hidden: true
+  }
+];
 
 const router = new VueRouter({
   routes
---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px">
+      <h3>系统登录</h3>
+      <div>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model.number="ruleForm.age"></el-input>
+        </el-form-item>
+      </div>
+      <div>密码</div>
+      <div>确认和取消</div>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      ruleForm: {
+        username: '',
+        password: ''
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss"></style>
